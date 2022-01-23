a6212b2f0aa76396f97434f1d24c37390c4f8c6f
Ontos <josemiguel.mercado17@gmail.com>
2022-01-23 18:19:15 -0300

    Primeros tonos

--- Intervalizer.ino
+++ Intervalizer.ino
@@ -1,9 +1,16 @@
+#define BUZZER 9
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  tone(BUZZER, 880, 5000);
+  // noTone(BUZZER);
+  delay(4500);
+  tone(BUZZER, 880*2, 5000);
+  // noTone(BUZZER);
+  delay(4500);
 }
