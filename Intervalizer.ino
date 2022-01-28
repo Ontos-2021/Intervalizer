6ea257afabbfe883a3b6d0792dcafadd1ce0a39b
Ontos <josemiguel.mercado17@gmail.com>
2022-01-28 16:17:38 -0300

    Probando la generación de tonos y silencios

--- Intervalizer.ino
+++ Intervalizer.ino
@@ -14,24 +14,13 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // Leer el estado de los botones
-int  valor_1 = digitalRead(BOTON_1); // Lectura del Botón 1
-int  valor_2 = digitalRead(BOTON_2); // Lectura del Botón 2
-int  valor_3 = digitalRead(BOTON_3); // Lectura del Botón 3
-int  valor_4 = digitalRead(BOTON_4); // Lectura del Botón 4
-
-  // Evaluar si los valores son altos (HIGH) o bajos (LOW)
+  int valor_1 = digitalRead(BOTON_1);
   if (valor_1 == HIGH) {
-    tone(BUZZER, 440);
-  } else if (valor_2 == HIGH) {
-    tone(BUZZER, 523.25);
-  } else if (valor_3 == HIGH) {
-    tone(BUZZER, 659.25);
-  } else if (valor_4 == HIGH) {
-    tone(BUZZER, 880);
+    tone(BUZZER, 1760, 40);
+    delay(100);
   } else {
-    noTone(BUZZER);
+    tone(BUZZER, 880, 100);  
+    delay(200);
   }
+
 }
