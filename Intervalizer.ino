fd572b844027aff3518490135219cf73b726fcaf
Ontos <josemiguel.mercado17@gmail.com>
2022-01-28 17:34:41 -0300

    Logré reproducir intervalos al azar

--- Intervalizer.ino
+++ Intervalizer.ino
@@ -1,11 +1,48 @@
+// Primero definimos variables para las entradas físicas del Arduino y sus respectivos componentes.
 #define BUZZER 9
 #define BOTON_1 2 
 #define BOTON_2 4
 #define BOTON_3 7
 #define BOTON_4 8
 
+// Array con las frecuencias de las notas músicales. Son dos octavas, de La4 a La6
+double notas_musicales[] = {
+  440,
+  466.16,
+  493.88,
+  523.25,
+  554.37,
+  587.33,
+  622.25,
+  659.26,
+  698.46,
+  739.99,
+  783.99,
+  830.61,
+  880,
+  932.33,
+  987.77,
+  1046.5,
+  1108.73,
+  1174.66,
+  1244.51,
+  1318.51,
+  1396.91,
+  1479.98,
+  1567.98,
+  1661.22,
+  1760
+};
+
+// Definimos los intervalos disponibles. En este caso, sólo serán 3ra Mayor, 4ta Justa, 5ta Justa y 6ta mayor.
+double intervalos[] = {4, 5, 7, 9, -4, -5, -7, -9};
+
 void setup() {
   // put your setup code here, to run once:
+  
+  Serial.begin(9600); // Iniciamos la comunicación serial
+  
+  // Definimos los pines como entradas o salidas
   pinMode(BUZZER, OUTPUT);
   pinMode(BOTON_1, INPUT);
   pinMode(BOTON_2, INPUT);
@@ -14,13 +51,33 @@ void setup() {
 }
 
 void loop() {
-  int valor_1 = digitalRead(BOTON_1);
-  if (valor_1 == HIGH) {
-    tone(BUZZER, 1760, 40);
-    delay(100);
-  } else {
-    tone(BUZZER, 880, 100);  
-    delay(200);
-  }
+  // Por ahora no será muy sofisticado el sistema. Elegiré una nota del medio para que nunca me tire error al elegir la segunda nota.
+  // Primero elegimos la primera nota al azar
 
+  // Elegimos la primer nota al azar
+  int numero_random = random(9, 17); 
+  double primer_nota = notas_musicales[numero_random]; // En este caso, se podrá elegir desde la novena nota hasta la décimosexta.
+  Serial.print("Esta es la primer nota: ");
+  Serial.println(primer_nota);
+  
+  // Elegimos al azar el intervalo
+  int intervalo = intervalos[random(0,7)];
+  Serial.print("Este es el intervalo: ");
+  Serial.println(intervalo);
+
+  // Definimos la segunda nota en base al intervalo
+  double segunda_nota = notas_musicales[numero_random + intervalo];
+
+  Serial.print("Este es la segunda nota: ");
+  Serial.println(segunda_nota);
+
+  Serial.println("-------");
+
+ // Ahora emitimos los sonidos
+ tone(BUZZER, primer_nota, 200);
+ delay(400);
+ tone(BUZZER, segunda_nota, 200);
+ delay(400);
+
+ delay(2700); 
 }
--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 # Intervalizer
 Intervalizer es un proyecto basado en Arduino para entrenar el reconocimiento de intervalos musicales
 
-## Buzzer
+### Buzzer
 Para generar el sonido utilizamos un buzzer pasivo, el cual necesita de corriente y de una frecuencia de onda en particular. 
 A diferencia del *buzzer pasivo*, existe el *buzzer activo* el cual posee un oscilador incorporado por lo que sólo necesita corriente para generar un tono.
 
@@ -12,3 +12,10 @@ La función tone() nos permite generar un tono con el buzzer.
 
 `tone(pin, frequency)`  
 `tone(pin, frequency, duration)`
+
+## Referencias
+https://juegosrobotica.es/musica-con-arduino/#
+https://www.arduino.cc/reference/en/language/functions/random-numbers/random/
+https://www.arduino.cc/reference/en/language/functions/advanced-io/tone/
+https://www.arduino.cc/reference/en/language/variables/data-types/array/
+https://learn.parallax.com/tutorials/robot/shield-bot/robotics-board-education-shield-arduino/chapter-4-boe-shield-bot-4#:~:text=Using%20the%20sizeof%20Function&text=It%20can%20tell%20you%20both,of%20elements%20in%20the%20array.
